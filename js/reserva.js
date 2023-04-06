47c2505ff0b995debb31e74fb51d258ac063763a
Gustavo Almeida <gustavomatos2277@gmail.com>
2023-04-06 16:03:42 -0300

    Reserva
    Criação + estilização dos cards para adicionar e remover quantias da reserva.

---
+++ js/reserva.js
@@ -0,0 +1,19 @@
+function showModal_adc() {
+    var element = document.getElementById("modal_adc");
+    element.classList.add("show-modal");
+}
+
+function hideModal_adc() {
+    var element = document.getElementById("modal_adc");
+    element.classList.remove("show-modal");
+}
+
+function showModal_rem() {
+    var element = document.getElementById("modal_rem");
+    element.classList.add("show-modal");
+}
+
+function hideModal_rem() {
+    var element = document.getElementById("modal_rem");
+    element.classList.remove("show-modal");
+}
