c7f5e3999761872db6bfc7606934165a55681cfb
Gustavo Almeida <gustavomatos2277@gmail.com>
2023-04-06 17:30:38 -0300

    Planejamento
    Criação + estilização do card de planejamento

--- js/planejamento.js
+++ js/planejamento.js
@@ -7,3 +7,13 @@ function hideModal_vlrfx() {
     var element = document.getElementById("modal_vlrfx");
     element.classList.remove("show-modal");
 }
+
+function showModal_planejamento() {
+    var element = document.getElementById("modal_planejamento");
+    element.classList.add("show-modal");
+}
+
+function hideModal_planejamento() {
+    var element = document.getElementById("modal_planejamento");
+    element.classList.remove("show-modal");
+}
