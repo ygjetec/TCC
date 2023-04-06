019049a7074fa9979527c2ac5374a33dc51a0378
Gustavo Almeida <gustavomatos2277@gmail.com>
2023-04-06 16:59:43 -0300

    Planejamento
    Criação + estilização do card de valores fixos

---
+++ js/planejamento.js
@@ -0,0 +1,9 @@
+function showModal_vlrfx() {
+    var element = document.getElementById("modal_vlrfx");
+    element.classList.add("show-modal");
+}
+
+function hideModal_vlrfx() {
+    var element = document.getElementById("modal_vlrfx");
+    element.classList.remove("show-modal");
+}
