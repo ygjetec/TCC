f11e3d5d456fdc262e391939fb72130bb7b2dbf1
ygjetec <124316578+ygjetec@users.noreply.github.com>
2023-04-14 21:40:55 -0300

    Login
    Alteração da estrutura dos formulários

--- php/connect.php
+++ php/connect.php
@@ -1,1 +1,18 @@
-<!-- Conexão com o banco -->
+<?php
+$host = "localhost";
+$banco = "tcc";
+$usuario = "root";
+$senha = "";
+
+/* $host = "bdmakeendsmeet.mysql.dbaas.com.br";
+$banco = "bdmakeendsmeet";
+$usuario = "bdmakeendsmeet";
+$senha = "Makeendsmeet@0"; */
+
+$conn = mysqli_connect($host, $usuario, $senha, $banco);
+
+if (!$conn) {
+    die("Falha na Conexão: " . mysqli_connect_error());
+}
+
+?>
