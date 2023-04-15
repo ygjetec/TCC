783c8418544ef1698e0d997a550e7245c7e1b7c8
ygjetec <124316578+ygjetec@users.noreply.github.com>
2023-04-14 22:00:23 -0300

    inicio - salvar.php

---
+++ php/salvar.php
@@ -0,0 +1,21 @@
+<?php
+
+$conn = mysqli_connect($host, $usuario, $senha, $banco);
+
+$nome = $_POST["nome"];
+$email = $_POST["email"];
+$senha = $_POST["senha"];
+$datanasc = $_POST["datanasc"];
+$sexo = $_POST["sexo"];
+
+
+
+
+
+
+
+
+
+
+
+?>
